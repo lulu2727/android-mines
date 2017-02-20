91d4bc4889b1462d1b9ac12e9702210e3c00d011
Ludovic <ludovic.sinquin@gmail.com>
2017-02-20 14:04:24 +0100

    Exercice 4

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -25,14 +25,20 @@ public class LibraryActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Replace with startActivityForResult() to start BookActivity
-                Toast.makeText(LibraryActivity.this, R.string.toast_todo, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LibraryActivity.this, BookActivity.class);
+                startActivityForResult(intent, 1);
+
+
+//                Toast.makeText(LibraryActivity.this, R.string.toast_todo, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO get back book name
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Toast.makeText(LibraryActivity.this, data.getStringExtra("bookName"), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
