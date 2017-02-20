32267f9e2b409840248cf0f3fa894f6f5b1b0f56
Ludovic <ludovic.sinquin@gmail.com>
2017-02-20 14:14:18 +0100

    Exercice 5

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -24,6 +24,7 @@ public class LibraryActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         // TODO save check box state
+//        outState.putBoolean("checkBox", checkBox.isChecked());
 
         super.onSaveInstanceState(outState);
     }
@@ -33,6 +34,7 @@ public class LibraryActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
 
         // TODO restore check box
+//        checkBox.setChecked(savedInstanceState.getBoolean("checkBox"));
     }
 
     @Override
